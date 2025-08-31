6f7abf4682d679daaa27bc3890db516f4c200a61
Koushik Chatterjee <koushikc125@gmail.com>
2025-09-01 00:58:10 +0530

    add ComponentInfo

--- app/src/main/java/com/example/portfolio/screen/data/ComponentInfo.kt
+++ app/src/main/java/com/example/portfolio/screen/data/ComponentInfo.kt
@@ -4,26 +4,31 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import com.example.portfolio.components.CardDesign
+import com.example.portfolio.components.GradientIcon
 import com.example.portfolio.core.util.Soon
 
 object ComponentInfo {
 
     val titles = listOf(
         "Minimal Card Design",
+        "Coming Soon",
         "Coming Soon"
     )
 
     val descriptions = listOf(
         "A minimal card design with path animation.",
+        "Coming Soon",
         "Coming Soon"
     )
     val time = listOf(
         "27.8.25",
+        "soon",
         "soon"
     )
 
     val content: List<@Composable () -> Unit> = listOf(
         { CardDesign() },
+        { GradientIcon() },
         { Soon() }
     )
     val componentsInfo = titles.mapIndexed { idx, title ->
